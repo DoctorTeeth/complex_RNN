9a9d0d588276a25dc256a8b01228474a1d8b2336
Augustus Odena <odenaaugustus@gmail.com>
2015-12-10 14:36:45 -0500

    functionize plotter code

--- plotter.py
+++ plotter.py
@@ -15,30 +15,29 @@ and ys (the cost at that step) and the label for the graph.
 So the one task we have is:
 read this out and generate the plot.
 """
+def plotter(files):
+
+    for f in files:
+        with open(f, 'rb') as pickle_file:
+            read_vals = cPickle.load(pickle_file)
+            train = read_vals['train_loss']
+            test  = read_vals['test_loss']
+
+            if show_test == "test":
+                y = test
+            else:
+                y = train
+            x = np.arange(len(y))
+
+            plt.plot(x, y, label=os.path.basename(f))
+
+    plt.xlabel("training sequences")
+    plt.ylabel("Cost")
+    plt.ylim(ymin=0)
+    plt.legend(loc='upper right')
+    plt.show()
 
 show_test = sys.argv[1]
 files = sys.argv[2:] # all other arguments are results files
-
 print files
-
-results = {}
-
-for f in files:
-  with open(f, 'rb') as pickle_file:
-        read_vals = cPickle.load(pickle_file)
-  train = read_vals['train_loss']
-  test  = read_vals['test_loss']
-
-  if show_test == "test":
-    y = test
-  else:
-    y = train
-  x = np.arange(len(y))
-
-  plt.plot(x, y, label=os.path.basename(f))
-
-plt.xlabel("training sequences")
-plt.ylabel("Cost")
-plt.ylim(ymin=0)
-plt.legend(loc='upper right')
-plt.show()
+plotter(files)
